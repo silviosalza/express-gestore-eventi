e38fa59723dd853a058bca0d853589685098964e
silviosalza <silviofilipposalza@gmail.com>
2023-11-17 10:29:16 +0100

    project scaffholding

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const eventRouter = require("./routers/eventRouter");
+
+dotenv.config();
+
+//creo istanza express
+const app = express();
+
+//registro il tipo di bodyparser
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+
+
+
+app.listen(process.env.PORT || 3000 , () =>{
+    console.log(`http://localhost:${process.env.PORT}`)
+});
